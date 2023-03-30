d5c0a507b601ea5d7a925e122a1ed2b812572dd5
Anderson_Veloso <99103223+AndersonVelos0@users.noreply.github.com>
2023-03-30 14:56:46 -0300

    Create trabalhando _Com_String.cpp
    Lendo e imprimindo uma variavel em C++

---
+++ trabalhando _Com_String.cpp
@@ -0,0 +1,12 @@
+//declara objetos para leitura e gravação de fluxos padrão
+#include <iostream>
+//caso não há nenhuma restrição, usará string 
+using namespace std;
+int main() {
+  //declarando uma string
+  string palavra;
+  //lendo uma string
+  cin >> palavra;
+  //imprimindo e concatenando
+  cout << "\nA palavra é:" << palavra;
+}
